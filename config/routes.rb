a402bb03898a40e4147f10b3a2d1093de79933f0
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-10-02 14:12:04 +0100

    Use shorter routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Lemmata::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/clients"
-  get "static_pages/who"
-  get "static_pages/what"
-  get "static_pages/contact"
+  match '/home',    to: 'static_pages#home',    via: 'get'
+  match '/clients', to: 'static_pages#clients', via: 'get'
+  match '/who',     to: 'static_pages#who',     via: 'get'
+  match '/what',    to: 'static_pages#what',    via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
