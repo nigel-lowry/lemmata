59c0efd45555c552a220f65101b1fac26ce14415
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-10-02 13:48:23 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lemmata::Application.config.secret_key_base = '06b20272c11d25eddc3e11b08be9a9965ed9da8a87ad8b01dfb1d7dfff03068c2ff972b19357dc1d7e941b7b525b44dcf00130abf36e73e844fa76b8ef8a70e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lemmata::Application.config.session_store :cookie_store, key: '_lemmata_session'
