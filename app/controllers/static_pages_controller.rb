22285baeba09b7a9c91242bbea226f468e6be7d0
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2013-10-02 13:54:35 +0100

    Add static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def clients
+  end
+
+  def who
+  end
+
+  def what
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Lemmata::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/clients"
+  get "static_pages/who"
+  get "static_pages/what"
+  get "static_pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
